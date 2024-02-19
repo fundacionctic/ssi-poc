93db61368949bf21730607af14d99f5b15e0e8d5
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2024-02-19 13:36:23 +0100

    ♻️ Refactor to improve clarity

--- waltid_client/main.py
+++ waltid_client/main.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import pprint
+from dataclasses import dataclass
 from typing import Dict, List, Tuple, Union
 from urllib.parse import parse_qs, unquote, urlparse
 
@@ -56,16 +57,6 @@ def get_first_wallet_id(wallet_api_base_url: str, wallet_token: str) -> str:
     res_accounts_json = res_accounts.json()
     logger.info(res_accounts_json)
     return res_accounts_json["wallets"][0]["id"]
-
-
-def get_first_did(wallet_api_base_url: str, wallet_token: str, wallet_id: str) -> str:
-    headers = {"Authorization": "Bearer " + wallet_token}
-    url_dids = wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/dids"
-    res_dids = requests.get(url_dids, headers=headers)
-    res_dids.raise_for_status()
-    res_dids_json = res_dids.json()
-    logger.info(pprint.pformat(res_dids_json))
-    return res_dids_json[0]["did"]
 
 
 def get_jwk_key(
@@ -329,9 +320,13 @@ def create_wallet_user(wallet_api_base_url: str, name: str, email: str, password
 
 
 def generate_key(
-    wallet_api_base_url: str, wallet_token: str, algo: str = "RSA"
+    wallet_api_base_url: str,
+    wallet_token: str,
+    algorithm: str = "RSA",
+    wallet_id: str = None,
+    alias: str = None,
 ) -> Tuple[str, str, str]:
-    wallet_id = get_first_wallet_id(wallet_api_base_url, wallet_token)
+    wallet_id = wallet_id or get_first_wallet_id(wallet_api_base_url, wallet_token)
 
     url_create_key = (
         wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/keys/generate"
@@ -340,7 +335,7 @@ def generate_key(
     headers = {"Authorization": "Bearer " + wallet_token}
 
     res_create_key = requests.post(
-        url_create_key, headers=headers, params={"algorithm": algo}
+        url_create_key, headers=headers, params={"algorithm": algorithm}
     )
 
     res_create_key.raise_for_status()
@@ -353,9 +348,12 @@ def generate_key(
         wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/dids/create/key"
     )
 
-    res_create_did = requests.post(
-        url_create_did, headers=headers, params={"keyId": key_id}
-    )
+    params = {"keyId": key_id}
+
+    if alias:
+        params["alias"] = alias
+
+    res_create_did = requests.post(url_create_did, headers=headers, params=params)
 
     try:
         res_create_did.raise_for_status()
@@ -369,8 +367,21 @@ def generate_key(
 
 
 def import_key(wallet_api_base_url: str, wallet_token: str, wallet_id: str, jwk: dict):
-    url_import = wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/keys/import"
     headers = {"Authorization": "Bearer " + wallet_token}
+
+    url_list = wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/keys"
+    res_list = requests.get(url_list, headers=headers)
+    res_list.raise_for_status()
+
+    exists = next(
+        (item for item in res_list.json() if item["keyId"]["id"] == jwk["kid"]), None
+    )
+
+    if exists:
+        logger.info("Key '%s' already exists in wallet '%s'", jwk["kid"], wallet_id)
+        return
+
+    url_import = wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/keys/import"
     res_import = requests.post(url_import, headers=headers, json=jwk)
 
     try:
@@ -397,18 +408,6 @@ def list_credentials(
     logger.info(pprint.pformat(list_creds))
 
     return list_creds
-
-
-def get_did_key_from_key_id(
-    wallet_api_base_url: str, wallet_token: str, wallet_id: str, key_id: str
-) -> str:
-    logger.debug("Looking for DID (key=%s) (wallet=%s)", key_id, wallet_id)
-    headers = {"Authorization": "Bearer " + wallet_token}
-    url_dids = wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/dids"
-    res_dids = requests.get(url_dids, headers=headers)
-    res_dids.raise_for_status()
-    dids = res_dids.json()
-    return next(item for item in dids if item["keyId"] == key_id)["did"]
 
 
 def debug_wallet(wallet_api_base_url: str, wallet_token: str):
@@ -447,6 +446,73 @@ def debug_wallet(wallet_api_base_url: str, wallet_token: str):
         logger.debug(pprint.pformat(creds))
 
 
+@dataclass
+class WalletUser:
+    wallet_api_base_url: str
+    token: str
+    wallet_id: str
+    key_id: str
+    did: str
+
+    def get_key_as_jwk(self) -> dict:
+        return get_jwk_key(
+            wallet_api_base_url=self.wallet_api_base_url,
+            wallet_id=self.wallet_id,
+            key_id=self.key_id,
+            wallet_token=self.token,
+        )
+
+
+def build_wallet_user(
+    wallet_api_base_url: str, email: str, password: str, key_alias: str = "ssi-poc"
+) -> WalletUser:
+    token = auth_login_wallet(
+        wallet_api_base_url=wallet_api_base_url, email=email, password=password
+    )
+
+    wallet_id = get_first_wallet_id(wallet_api_base_url, token)
+
+    url_dids = wallet_api_base_url + f"/wallet-api/wallet/{wallet_id}/dids"
+    headers = {"Authorization": "Bearer " + token}
+    res_dids = requests.get(url_dids, headers=headers)
+    res_dids.raise_for_status()
+    dids = res_dids.json()
+    did_dict = next((item for item in dids if item["alias"] == key_alias), None)
+
+    if did_dict:
+        logger.info(
+            "Found existing DID for alias '%s' (url=%s)", key_alias, wallet_api_base_url
+        )
+
+        key_id = did_dict["keyId"]
+        did = did_dict["did"]
+    else:
+        logger.info(
+            "Generating new key for alias '%s' (url=%s)", key_alias, wallet_api_base_url
+        )
+
+        wid, key_id, did = generate_key(
+            wallet_api_base_url=wallet_api_base_url,
+            wallet_token=token,
+            wallet_id=wallet_id,
+            alias=key_alias,
+        )
+
+        assert wid == wallet_id
+
+    wallet_user = WalletUser(
+        wallet_api_base_url=wallet_api_base_url,
+        token=token,
+        wallet_id=wallet_id,
+        key_id=key_id,
+        did=did,
+    )
+
+    logger.debug("Wallet user (%s): %s", wallet_api_base_url, wallet_user)
+
+    return wallet_user
+
+
 def main():
     cfg = environ.to_config(AppConfig)
     logger.info(cfg)
@@ -476,48 +542,35 @@ def main():
 
     logger.info("Logging in wallet user: %s", cfg.wallet_anchor_user_email)
 
-    anchor_wallet_token = auth_login_wallet(
-        cfg.wallet_anchor_api_base_url,
-        cfg.wallet_anchor_user_email,
-        cfg.wallet_anchor_user_password,
+    anchor_wallet_user = build_wallet_user(
+        wallet_api_base_url=cfg.wallet_anchor_api_base_url,
+        email=cfg.wallet_anchor_user_email,
+        password=cfg.wallet_anchor_user_password,
     )
 
     logger.info("Logging in wallet user: %s", cfg.wallet_provider_user_email)
 
-    provider_wallet_token = auth_login_wallet(
-        cfg.wallet_provider_api_base_url,
-        cfg.wallet_provider_user_email,
-        cfg.wallet_provider_user_password,
+    provider_wallet_user = build_wallet_user(
+        wallet_api_base_url=cfg.wallet_provider_api_base_url,
+        email=cfg.wallet_provider_user_email,
+        password=cfg.wallet_provider_user_password,
     )
 
     logger.info("Logging in wallet user: %s", cfg.wallet_consumer_user_email)
 
-    consumer_wallet_token = auth_login_wallet(
-        cfg.wallet_consumer_api_base_url,
-        cfg.wallet_consumer_user_email,
-        cfg.wallet_consumer_user_password,
+    consumer_wallet_user = build_wallet_user(
+        wallet_api_base_url=cfg.wallet_consumer_api_base_url,
+        email=cfg.wallet_consumer_user_email,
+        password=cfg.wallet_consumer_user_password,
     )
 
-    logger.info("Generating signing key")
+    logger.info("Signing key wallet ID: %s", anchor_wallet_user.wallet_id)
+    logger.info("Signing key ID: %s", anchor_wallet_user.key_id)
+    logger.info("Signing key DID: %s", anchor_wallet_user.did)
 
-    signing_key_wallet_id, signing_key_id, signing_key_did = generate_key(
-        wallet_api_base_url=cfg.wallet_anchor_api_base_url,
-        wallet_token=anchor_wallet_token,
-        algo=cfg.signing_key_algorithm,
-    )
+    issuer_signing_jwk = anchor_wallet_user.get_key_as_jwk()
 
-    logger.info("Signing key wallet ID: %s", signing_key_wallet_id)
-    logger.info("Signing key ID: %s", signing_key_id)
-    logger.info("Signing key DID: %s", signing_key_did)
-    logger.info("Exporting signing key to JWK")
-
-    issuer_signing_jwk = get_jwk_key(
-        wallet_api_base_url=cfg.wallet_anchor_api_base_url,
-        wallet_id=signing_key_wallet_id,
-        key_id=signing_key_id,
-        wallet_token=anchor_wallet_token,
-    )
-
+    logger.info("Signing key in JWK format:\n%s", pprint.pformat(issuer_signing_jwk))
     logger.info("Loading VC from disk: %s", cfg.vc_path)
 
     vc = json.load(open(cfg.vc_path))
@@ -528,57 +581,45 @@ def main():
         jwk=issuer_signing_jwk,
         vc=vc,
         issuer_api_base_url=cfg.issuer_api_base_url,
-        issuer_did=signing_key_did,
-    )
-
-    consumer_wallet_id = get_first_wallet_id(
-        cfg.wallet_consumer_api_base_url, consumer_wallet_token
-    )
-
-    provider_wallet_id = get_first_wallet_id(
-        cfg.wallet_provider_api_base_url, provider_wallet_token
-    )
-
-    provider_user_did_key = get_first_did(
-        cfg.wallet_provider_api_base_url, provider_wallet_token, provider_wallet_id
+        issuer_did=anchor_wallet_user.did,
     )
 
     logger.info("Importing signing key to consumer and provider wallets")
 
     import_key(
-        wallet_api_base_url=cfg.wallet_consumer_api_base_url,
-        wallet_token=consumer_wallet_token,
-        wallet_id=consumer_wallet_id,
+        wallet_api_base_url=consumer_wallet_user.wallet_api_base_url,
+        wallet_token=consumer_wallet_user.token,
+        wallet_id=consumer_wallet_user.wallet_id,
         jwk=issuer_signing_jwk,
     )
 
     import_key(
-        wallet_api_base_url=cfg.wallet_provider_api_base_url,
-        wallet_token=provider_wallet_token,
-        wallet_id=provider_wallet_id,
+        wallet_api_base_url=provider_wallet_user.wallet_api_base_url,
+        wallet_token=provider_wallet_user.token,
+        wallet_id=provider_wallet_user.wallet_id,
         jwk=issuer_signing_jwk,
     )
 
-    logger.info("Using offer request with recipient user: %s", provider_user_did_key)
+    logger.info("Using offer request with recipient user: %s", provider_wallet_user.did)
 
     use_offer_request(
-        wallet_api_base_url=cfg.wallet_provider_api_base_url,
-        wallet_id=provider_wallet_id,
-        user_did_key=provider_user_did_key,
-        wallet_token=provider_wallet_token,
+        wallet_api_base_url=provider_wallet_user.wallet_api_base_url,
+        wallet_id=provider_wallet_user.wallet_id,
+        user_did_key=provider_wallet_user.did,
+        wallet_token=provider_wallet_user.token,
         credential_offer_url=credential_offer_url,
     )
 
     logger.info(
         "Listing credentials for wallet %s (wallet_id=%s)",
         cfg.wallet_provider_api_base_url,
-        provider_wallet_id,
+        provider_wallet_user.wallet_id,
     )
 
     list_credentials(
-        wallet_api_base_url=cfg.wallet_provider_api_base_url,
-        wallet_token=provider_wallet_token,
-        wallet_id=provider_wallet_id,
+        wallet_api_base_url=provider_wallet_user.wallet_api_base_url,
+        wallet_token=provider_wallet_user.token,
+        wallet_id=provider_wallet_user.wallet_id,
     )
 
     request_credentials = ["UniversityDegreeCredential"]
@@ -592,25 +633,26 @@ def main():
     openid4vp_authorize_url = get_openid4vp_presentation_request_url(
         verifier_api_base_url=cfg.verifier_api_base_url,
         request_credentials=request_credentials,
-        allowed_issuer_dids=signing_key_did,
+        allowed_issuer_dids=anchor_wallet_user.did,
     )
 
     logger.info("Got OpenID4VP presentation request URL: %s", openid4vp_authorize_url)
 
     logger.info(
-        "Looking for matching credentials in %s", cfg.wallet_provider_api_base_url
+        "Looking for matching credentials in %s",
+        provider_wallet_user.wallet_api_base_url,
     )
 
     matching_creds = find_matching_credentials(
         presentation_url=openid4vp_authorize_url,
-        wallet_api_base_url=cfg.wallet_provider_api_base_url,
-        wallet_token=provider_wallet_token,
-        wallet_id=provider_wallet_id,
+        wallet_api_base_url=provider_wallet_user.wallet_api_base_url,
+        wallet_token=provider_wallet_user.token,
+        wallet_id=provider_wallet_user.wallet_id,
         latest_only=True,
     )
 
     logger.info("Found matching credentials:\n%s", pprint.pformat(matching_creds))
-    logger.info("Sharing credentials from %s", cfg.wallet_provider_api_base_url)
+    logger.info("Sharing credentials from %s", provider_wallet_user.wallet_api_base_url)
 
     status_before = get_verification_status(
         verifier_api_base_url=cfg.verifier_api_base_url,
@@ -621,12 +663,12 @@ def main():
     assert not status_before.get("verificationResult")
 
     share_credentials(
-        signing_did_key=signing_key_did,
+        signing_did_key=anchor_wallet_user.did,
         presentation_url=openid4vp_authorize_url,
         shared_credential_ids=[matching_creds["id"]],
-        wallet_api_base_url=cfg.wallet_provider_api_base_url,
-        wallet_token=provider_wallet_token,
-        wallet_id=provider_wallet_id,
+        wallet_api_base_url=provider_wallet_user.wallet_api_base_url,
+        wallet_token=provider_wallet_user.token,
+        wallet_id=provider_wallet_user.wallet_id,
     )
 
     status_after = get_verification_status(
